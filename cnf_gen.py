77701b443432dd0f2866233d7445af7fe88edffd
blackle <isabelle@blackle-mori.com>
2018-07-07 19:31:01 -0400

    invoke minisat from cnf generator, type safety, prime factorization

--- cnf_gen.py
+++ cnf_gen.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
-from typing import List, Tuple
+from typing import List, Tuple, IO
+import subprocess
 
 #todo:
-# automatic invocation of minisat
-# extraction of integer values from sat solution
 # automatic padding during addition/multiplication
 # find and restrict overflow from happening
 # optimized squaring clauses/addition??
@@ -14,11 +13,25 @@ class CNFVariable:
 	def __init__(self, val : int) -> None:
 		self.__val = val #type: int
 
+	#this sucks and makes no sense
+	def __eq__(self, other : object) -> bool:
+		if not isinstance(other, CNFVariable):
+			return NotImplemented
+
+		return abs(self.__val) == abs(other.__val)
+
+	def __len__(self) -> bool:
+		if self.__val > 0:
+			return 1
+		else:
+			return 0
+
 	def __invert__(self) -> 'CNFVariable':
 		return CNFVariable(-self.__val)
 
 	def __str__(self) -> str:
 		return str(self.__val)
+
 
 class CNFFormula:
 	def __init__(self) -> None:
@@ -33,13 +46,65 @@ class CNFFormula:
 	def add(self, clause : List[CNFVariable]) -> None:
 		self.__clauses.append(clause)
 
-	def output(self) -> None:
-		print("p cnf %d %d" % (self.__maxvar - 1, len(self.__clauses)))
+	def write(self, fd : IO[bytes]) -> None:
+		fd.write(("p cnf %d %d\n" % (self.__maxvar - 1, len(self.__clauses))).encode('ascii'))
 		for clause in self.__clauses:
-			print(" ".join(str(x) for x in clause) + " 0")
+			fd.write((" ".join(str(x) for x in clause) + " 0\n").encode('ascii'))
+
 
 class SATSolver:
-	def __init__(self, )
+	def __init__(self, cnf : CNFFormula) -> None:
+		self.__cnf = cnf
+		self.__solver_invoked = False #type: bool
+		self.__satisfiable = False #type: bool
+		self.__solution = [] #type: List[CNFVariable]
+
+	def solve(self) -> None:
+		print("Invoking sat solver...")
+		minisat = subprocess.Popen(["minisat", "/dev/stdin", "/dev/stderr"], stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+
+		self.__cnf.write(minisat.stdin)
+		minisat.stdin.close()
+
+		#output is like 'SAT\n1 -2 -3 4 5 0\n' or 'UNSAT\n'
+		satisfiable = minisat.stderr.readline()[:-1].decode('ascii')
+		self.__satisfiable = (satisfiable == "SAT")
+
+		if self.__satisfiable:
+			solution = minisat.stderr.readline()[:-1].decode('ascii')
+			solution = solution.strip().split(" ")[:-1]
+
+			for var in solution:
+				self.__solution.append(CNFVariable(int(var)))
+
+		self.__solver_invoked = True
+
+	def satisfiable(self) -> bool:
+		assert(self.__solver_invoked)
+		return self.__satisfiable
+
+	def varlist_to_int(self, varlist : List[CNFVariable]) -> int:
+		assert(self.__solver_invoked)
+		assert(self.__satisfiable)
+
+		bitvector = [] #type: List[bool]
+
+		for var in varlist:
+			for solvar in self.__solution:
+				if var == solvar:
+					bitvector.append(bool(solvar))
+
+					break
+
+		assert(len(bitvector) == len(varlist))
+
+		intval = 0 #type: int
+
+		for i in range(len(bitvector)):
+			if bitvector[i]:
+				intval += pow(2, i)
+
+		return intval
 
 #code from https://blog.lse.epita.fr/articles/24-using-sat-and-smt-to-defeat-simple-hashing-algorit.html
 def cnf_int(cnf : CNFFormula, bits : int) -> List[CNFVariable]:
@@ -53,6 +118,9 @@ def cnf_equal(cnf : CNFFormula, n : List[CNFVariable], c : int) -> None:
 			cnf.add([n[i]])
 		else:
 			cnf.add([~n[i]])
+
+	if c > 0:
+		print("WARNING: overflow in cnf_equal!")
 
 def cnf_1bitadder(cnf : CNFFormula, a : CNFVariable, b : CNFVariable, c : CNFVariable) -> Tuple[CNFVariable, CNFVariable]:
 	res = cnf.new_var()
@@ -119,7 +187,7 @@ def cnf_mult(cnf : CNFFormula, a : List[CNFVariable], b : List[CNFVariable]) -> 
 		ppt.append(pptrow)
 
 	out = ppt[0][:]
-	#add a silly little padding variable so we can do the adding loop
+	#useless, pitiful variable...
 	dumbo = cnf.new_var()
 	cnf.add([~dumbo])
 	out.append(dumbo)
@@ -135,28 +203,39 @@ def cnf_mult(cnf : CNFFormula, a : List[CNFVariable], b : List[CNFVariable]) -> 
 if __name__ == "__main__":
 	cnf = CNFFormula()
 
-	bitdepth = 32
+	bitdepth = 24
 	a = cnf_int(cnf, bitdepth)
 	b = cnf_int(cnf, bitdepth)
-	# c = cnf_int(cnf, bitdepth)
 
-	a2 = cnf_mult(cnf, a, a)
-	b2 = cnf_mult(cnf, b, b)
-	# c2 = cnf_mult(cnf, c, c)
+	ab = cnf_mult(cnf, a, b)
 
-	a2b2 = cnf_add(cnf, a2, b2)
-	# a2b2c2 = cnf_add(cnf, a2b2, c2)
+	#ensure neither a nor b is 1, i.e. at least one bit other than the first is set
+	cnf.add(a[1:])
+	cnf.add(b[1:])
 
-	cnf.add(a)
-	cnf.add(b)
-	# cnf.add(c)
+	aint = 67589
+	bint = 15485867
+	abint = aint*bint
 
-	# cnf_equal(cnf, a, 0b0111)
-	# cnf_equal(cnf, b, 0b0111)
-	cnf_equal(cnf, a2b2, 0b11111010001011111111010100000010)
+	print("a: " + str(aint))
+	print("b: " + str(bint))
+	print("ab: " + str(abint))
 
-	print("c a = " + " ".join(str(x) for x in a))
-	print("c b = " + " ".join(str(x) for x in b))
-	# print("c c = " + " ".join(str(x) for x in c))
+	# cnf_equal(cnf, a, aint)
+	# cnf_equal(cnf, b, bint)
+	cnf_equal(cnf, ab, abint)
 
-	cnf.output()
+	solver = SATSolver(cnf)
+	solver.solve()
+	print("satisfiable: " + str(solver.satisfiable()))
+
+	if (solver.satisfiable()):
+		aint = solver.varlist_to_int(a)
+		bint = solver.varlist_to_int(b)
+		# a2b2int = solver.varlist_to_int(a2b2)
+
+		assert(aint*bint == abint)
+
+		print("a: " + str(aint))
+		print("b: " + str(bint))
+		print("ab: " + str(abint))
